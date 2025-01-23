732fe8ed1475b93546458043e618edcaec97413d
Adaobilynda1234 <adaobilynda1234@gmail.com>
2025-01-23 21:02:48 +0100

    modified home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -110,7 +110,7 @@ const Home = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center">
             <h2 className="text-3xl font-extrabold text-gray-900">
-              Powerful Features
+              Powerful Feature
             </h2>
           </div>
 
